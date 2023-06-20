7b67ff4402a10286ff36eb6c898c6b6147af9f19
Urs <st168124@stud.uni-stuttgart.de>
2023-06-20 18:45:13 +0200

    some drama statisticss and an example file

---
+++ drama_stats.py
@@ -0,0 +1,32 @@
+import xml.etree.ElementTree as ET
+import os
+
+drama_stats = {}
+
+for drama_file in os.listdir('data'):
+    tree = ET.parse(os.path.join('data', drama_file))
+    root = tree.getroot()
+
+    id_no = root.find('.//{http://www.tei-c.org/ns/1.0}idno').text
+    print(id_no)
+
+    title_stmt = root.find('.//{http://www.tei-c.org/ns/1.0}fileDesc/{http://www.tei-c.org/ns/1.0}titleStmt')
+    print(title_stmt.findall('./*'))
+    title = title_stmt.find('./{http://www.tei-c.org/ns/1.0}title[@type="main"]').text
+    subtitle = title_stmt.find('./{http://www.tei-c.org/ns/1.0}title[@type="sub"]').text
+    author = title_stmt.findall('.//{http://www.tei-c.org/ns/1.0}author/{http://www.tei-c.org/ns/1.0}persName/*')
+    author = " ".join([author_part.text for author_part in author])
+
+    # currently getting publication, not premiere
+    year = root.find('.//{http://www.tei-c.org/ns/1.0}standOff//{http://www.tei-c.org/ns/1.0}event[@type="print"]').get('when')
+
+    num_scenes = len(root.findall('.//{http://www.tei-c.org/ns/1.0}div[@type="scene"]'))
+    num_lines = len(root.findall('.//{http://www.tei-c.org/ns/1.0}div[@type="scene"]//{http://www.tei-c.org/ns/1.0}p'))
+
+    num_stage_dirs = len(root.findall('.//{http://www.tei-c.org/ns/1.0}stage'))
+
+    drama_stats[id_no] = {"title": title, "author": author, "year": year, "num_scenes": num_scenes, "num_lines": num_lines,
+                          "num_stage_dirs": num_stage_dirs,  }
+
+
+print(drama_stats)
