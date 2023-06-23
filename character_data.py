b1702041b3df611d7467a681420d1be7e50670c0
Joshua C Rivera <39106451+JoshuaCRivera@users.noreply.github.com>
2023-06-23 21:24:00 +0200

    Add more character stats

--- character_data.py
+++ character_data.py
@@ -1,28 +1,88 @@
 import xml.etree.ElementTree as ET
-import re
 
-tree = ET.parse('data.xml')
-root = tree.getroot()
+def character_stats(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
 
-speakers = root.findall('.//speaker')
-persons = root.findall('.//person')
-association = root.findall('.//listPerson//relation')
+    namespace = {'tei': 'http://www.tei-c.org/ns/1.0'}
 
-active_passive = []
+    characters = []
+    for name in root.findall(".//tei:persName", namespace):
+        characters.append(name.text)
 
-for x in association:
-    active_passive.append([x.attrib['active'], x.attrib['passive']])
+    profile_desc = root.find(".//tei:profileDesc", namespace)
+    relations = profile_desc.findall(".//tei:relation", namespace)
 
-character_names = []
+    relationships = []
+    for relation in relations:
+        relationship_type = relation.attrib["name"]
+        mutual = relation.attrib.get("mutual", "").split()
+        active = relation.attrib.get("active", "").split()
+        passive = relation.attrib.get("passive", "").split()
 
-for speaker in speakers:
-    character_names.append(speaker.text)
+        relationships.append((relationship_type, mutual, active, passive))
 
-total_characters = len(character_names)
+    longest_length = 0
+    longest_speaker = ''
+    longest_line = ''
 
-print("Total unique dialogues:", total_characters)
-unique_names = []
-for name in persons:
-    unique_names.append(name[0].text)
-print("All character names:", unique_names)
-print("All relationships, active -> passive:", active_passive)
+    shortest_length = 100
+    shortest_speaker = ''
+    shortest_line = ''
+
+    speaker_counts = {}
+
+    for sp in root.findall('.//tei:sp', namespace):
+        speaker = sp.find('tei:speaker', namespace).text
+        dialogue = ' '.join([line.text.strip() for line in sp.findall('tei:l', namespace)])
+
+        length = len(dialogue)
+
+        if length > longest_length:
+            longest_length = length
+            longest_speaker = speaker
+            longest_line = dialogue
+
+        if length < shortest_length:
+            shortest_length = length
+            shortest_speaker = speaker
+            shortest_line = dialogue
+
+        if speaker in speaker_counts:
+            speaker_counts[speaker] += 1
+        else:
+            speaker_counts[speaker] = 1
+
+    print("Named Characters:")
+    for c in sorted(characters):
+        print(c)
+    print()
+
+    print("Speaker Counts:")
+    for speaker, count in sorted(speaker_counts.items(), key=lambda x: x[1], reverse=True):
+        print(f"{speaker}: {count}")
+    print()
+
+    print("Relationships:")
+    for relationship, mutual, active, passive in relationships:
+        print("Relationship:", relationship)
+        print("Mutual:", mutual)
+        print("Active:", active)
+        print("Passive:", passive)
+        print()
+
+    print("Speaker of longest dialogue:", longest_speaker)
+    print("Length of longest dialogue:", longest_length)
+    # print("Longest dialogue:", longest_line)
+    print()
+
+    print("Speaker of shortest dialogue:", shortest_speaker)
+    print("Length of shortest dialogue:", shortest_length)
+    # print("Shortest dialogue:", shortest_line)
+
+# Example:
+filename = "tei/sachs-ein-comedi-von-dem-reichen-sterbenden-menschen-der-hecastus-genannt.xml"
+character_stats(filename)
+
+
+
