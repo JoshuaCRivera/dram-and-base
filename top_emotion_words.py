225474a9448556a16bbe79cc8aead6c8d1a3bb7e
Joshua C Rivera <39106451+JoshuaCRivera@users.noreply.github.com>
2023-06-23 18:27:15 +0200

    Add modification to improve runtime

--- top_emotion_words.py
+++ top_emotion_words.py
@@ -1,39 +1,46 @@
 import pandas as pd
 import numpy as np
-import spacy
 from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import nltk
 
-def get_top_words(filename):
-    nlp = spacy.load('de_core_news_sm')
-    
-    df = pd.read_csv(filename).dropna(subset=['Text'])
-    
-    stopwords_german = nlp.Defaults.stop_words
+def remove_stop_words(text):
+    stop_words = set(stopwords.words('german'))
     
     # Simple catch for some dialectal spellings
-    stopwords_german.update([word[:-1] for word in nlp.Defaults.stop_words])
+    stop_words.update(set([word[:-1] for word in stop_words]))
     
-    df['Text'] = df['Text'].apply(lambda x: ' '.join([token.text.lower() for token in nlp(x) if token.text.lower() not in stopwords_german]))
+    tokens = word_tokenize(text, language='german')
+    filtered_tokens = [token for token in tokens if token.lower() not in stop_words and len(token) > 2]
     
+    return ' '.join(filtered_tokens)
+
+def get_top_words(filename):
+    df = pd.read_csv(filename).dropna(subset=['Text'])
+
+    df['Text'] = df['Text'].apply(remove_stop_words)
+
     vectorizer = TfidfVectorizer(max_features=1000)
     tfidf_matrix = vectorizer.fit_transform(df['Text'])
-    
+
     feature_names = vectorizer.get_feature_names_out()
-    
+
     emotion_tags = df['Emotion'].unique()
     for tag in emotion_tags:
         concatenated_text = ' '.join(df[df['Emotion'] == tag]['Text'])
-        
+
         tfidf_vector = vectorizer.transform([concatenated_text])
-        
+
         tfidf_scores = tfidf_vector.toarray()[0]
-        
-        top_word_indices = np.argsort(tfidf_scores)[-10:][::-1]
-        
+
+        top_word_indices = np.argsort(tfidf_scores)[-15:][::-1]
+
         top_words = [feature_names[idx] for idx in top_word_indices]
-        
+
         print(f"Emotion Tag: {tag}")
-        print(f"Top 10 Most Informative Emotion Words: {top_words}")
+        print(f"Top 15 Most Informative Emotion Words: {top_words}")
         print()
-        
+
 get_top_words('dialogue_data.csv')
+
